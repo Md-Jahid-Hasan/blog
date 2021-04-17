44e2897c0c8d961f21d3bf32d7cd80c35506bb5a
Md-Jahid-Hasan <jahid15-1905@gmail.com>
2021-04-17 15:04:53 +0600

    upload full project

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>', views.post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_details, name='post_details'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('search/', views.post_search, name='post_search')
+]
